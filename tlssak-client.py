46c25e6541484753d6953b8f58a7218f96c771fc
Mirko Hansen <hansen@rbt-nbg.de>
2016-11-08 14:41:55 +0100

    choose SSL/TLS version via command line

--- tlssak-client.py
+++ tlssak-client.py
@@ -5,6 +5,7 @@ import sys
 # TLS SAK imports
 from lib.connection import Connection_Exception
 from lib.connection.tcpsocket import Connection_TCP_Socket
+from lib.tls import TLS_VERSIONS
 from lib.tls.tlsciphersuites import TLS_CipherSuite_Database
 from lib.tls.tlscompressionmethods import TLS_CompressionMethod_Database
 from lib.tls.tlsconnection import TLS_Connection
@@ -21,7 +22,7 @@ def client(args):
         while True:
             with connection:
                 tls_connection = TLS_Connection(connection)
-                tls_connection.setClientProtocolVersion('TLSv1.2')
+                tls_connection.setClientProtocolVersion(args.tlsprotocol)
                 tls_connection.setAvailableCipherSuites(cipher_suites)
                 tls_connection.setAvailableCompressionMethods(TLS_CompressionMethod_Database.getInstance().getAllCompressionMethods())
                 tls_connection.connect()
@@ -41,6 +42,7 @@ def client(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--starttls', help='use STARTTLS for specific protocol', choices=starttls_supported, dest='starttls')
+    parser.add_argument('-tp', '--tls-protocol', default='TLSv1', help='choose protocol to connect with', choices=TLS_VERSIONS, dest='tlsprotocol')
     parser.add_argument('-p', '--port', type=int, default=443, help='TCP port to be checked', dest='port')
     parser.add_argument('host', help='hostname or IP address of target system')
     args = parser.parse_args()
