5f63dbc6e89e96cb672bfb44850253163039c4ed
BaaaZen <baaazen@gmail.com>
2017-01-17 16:26:11 +0100

    removed import for debug

--- lib/certificate/x509certificate.py
+++ lib/certificate/x509certificate.py
@@ -15,9 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# temp
-import binascii
-
 from lib.certificate import asn1
 from lib.certificate import signature
 from lib.certificate.asn1structs import x509
